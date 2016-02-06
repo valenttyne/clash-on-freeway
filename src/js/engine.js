a255346bd736245042f618e96efd131f420dd412
valenttyne <valenttyne@gmail.com>
2016-02-06 12:45:29 -0200

    configurando os inimigos

--- src/js/engine.js
+++ src/js/engine.js
@@ -6,6 +6,10 @@ function start(){
 	//adiciona a div fundo o jogador e os inimigos
 	$('#fundo').append("<div id='player'></div>");
 	$('#fundo').append("<div id='en01'></div>");
+	$('#fundo').append("<div id='en02'></div>");
+	$('#fundo').append("<div id='en03'></div>");
+	$('#fundo').append("<div id='en04'></div>");
+	$('#fundo').append("<div id='en05'></div>");
 
 
 }
